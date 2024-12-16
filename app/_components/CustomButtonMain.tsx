83d596427ff7cfdee39676d7e072b70ffa10f003
MichihideS <michihides@gmail.com>
2024-12-17 00:56:40 +0100

    add home, login and register page endpoints + a custombutton with void function

---
+++ app/_components/CustomButtonMain.tsx
@@ -0,0 +1,16 @@
+export default function CustomButtonMain({
+	title,
+	myFunction,
+}: {
+	title: string
+	myFunction: () => void
+}) {
+	return (
+		<button
+			onClick={myFunction}
+			className="p-4 bg-sky-500 font-bold hover:bg-sky-400"
+		>
+			{title}
+		</button>
+	)
+}
---
+++ app/home/page.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+//import { useEffect } from "react"
+import CustomButtonMain from "../_components/CustomButtonMain"
+
+export default function Home() {
+	function logTest() {
+		console.log("WHY")
+	}
+
+	/*
+	useEffect(() => {
+		//test()
+	})
+
+	async function test() {
+		const response = await fetch("http://localhost:8080/api/v1/user")
+		const data = await response.json()
+
+		console.log(data)
+	}
+		*/
+
+	return (
+		<div>
+			<CustomButtonMain
+				title={"LogTest"}
+				myFunction={logTest}
+			></CustomButtonMain>
+		</div>
+	)
+}
